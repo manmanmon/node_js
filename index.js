6d317f34d9e67544c1ad84fe40c6d0aee31f8b70
manmanmon <burgoch-roman9a@mail.ru>
2021-12-21 14:12:52 +0300

    Сделал ДЗ № 3

--- index.js
+++ index.js
@@ -1,113 +1,36 @@
-const colors = require("colors/safe");
-const { DateTime } = require("luxon");
-const EventEmitter = require('events');
+const fs = require('fs');
+const readline = require('readline');
+const readStream = fs.createReadStream('./access.log', 'utf8');
+const writeStreamIP1 = fs.createWriteStream('./89.123.1.41.requests.log', 'utf8');
+const writeStreamIP2 = fs.createWriteStream('./34.48.240.111.requests.log', 'utf8');
 
-// Для запуска таймера введите команду 'npm run start 2022 12 25 23 45', где 2022 - год, 12 - месяц, 25 - день, 23 - час, 45 - минута (дата, время могут быть любыми)
+let firstIP = [];
+let secondIP = [];
 
-// Привожу введенные значения к числам 
-let parseIntMass = process.argv.map(function (item) {
-    let number = parseInt(item);
-    return isNaN(number) ? item : number;
+const rl = readline.createInterface({
+    input: readStream,
+    terminal: true,
 });
 
-const [, , myYear, myMonth, myDay, mayHour, myMinute] = parseIntMass;
+rl.on('line', (line) => {
+    if (line) {
+        let ip = line.match(/\d+\.\d+\.\d+\.\d+/g).toString();
 
-let currentDate = DateTime.now();
-
-myDate = DateTime.fromObject({
-    year: myYear,
-    month: myMonth,
-    day: myDay,
-    hour: mayHour,
-    minute: myMinute,
-    second: 0,
-    millisecond: currentDate.millisecond,
-});
-
-let diffDate = myDate.diff(currentDate, ['year', 'months', 'days', 'hours', 'minutes', 'seconds']).toObject();
-
-let {
-    years: yearsTmr,
-    months: monthsTmr,
-    days: daysTmr,
-    hours: hoursTmr,
-    minutes: minutesTmr,
-    seconds: secondsTmr,
-} = diffDate;
-
-// Создаю класс-наследник EventEmmitter и его экземпляра 
-class MyEmitter extends EventEmitter { };
-const emitterTimerConsole = new MyEmitter();
-
-// Функция таймера
-function setTimer() {
-
-    // Создаю класс для обработки событий в зависимости от их типа
-    class ConsoleLogMessage {
-        static showRemainingTime(params) {
-            console.log(params);
-        }
-        static sayTimeOut() {
-            console.log(colors.green('Время вышло!'));
+        switch (ip) {
+            case '89.123.1.41':
+                firstIP.push(line);
+                writeStreamIP1.write(`${line}\n`);
+                break;
+            case '34.48.240.111':
+                secondIP.push(line);
+                writeStreamIP2.write(`${line}\n`);
+                break;
+            default:
+                break;
         }
     }
+});
 
-    // Создаю обработчики событий
-    emitterTimerConsole.on('showRemainingTime', ConsoleLogMessage.showRemainingTime);
-    emitterTimerConsole.on('sayTimeOut', ConsoleLogMessage.sayTimeOut);
-    emitterTimerConsole.setMaxListeners(0);
-
-    let timer = setInterval(() => {
-        let timeForShow = `${yearsTmr} лет, ${monthsTmr} месяцев, ${daysTmr} дней, ${hoursTmr} часов, ${minutesTmr} минут, ${secondsTmr} секунд`;
-
-        if (yearsTmr == 0 && monthsTmr == 0 && daysTmr == 0 && hoursTmr == 0 && minutesTmr == 0 && secondsTmr == 0) {
-            emitterTimerConsole.emit('sayTimeOut');
-            // console.log('Время вышло');
-            clearInterval(timer);
-        }
-        if (secondsTmr == 0) {
-            if (minutesTmr > 0) {
-                --minutesTmr;
-                secondsTmr = '59';
-                emitterTimerConsole.emit('showRemainingTime', timeForShow);
-                // console.log(timeForShow);
-            } else if (minutesTmr == 0 && hoursTmr > 0) {
-                --hoursTmr;
-                minutesTmr = '59';
-                secondsTmr = '59';
-                emitterTimerConsole.emit('showRemainingTime', timeForShow);
-                // console.log(timeForShow);
-            } else if (minutesTmr == 0 && hoursTmr == 0 && daysTmr > 0) {
-                --daysTmr;
-                hoursTmr = '23';
-                minutesTmr = '59';
-                secondsTmr = '59';
-                emitterTimerConsole.emit('showRemainingTime', timeForShow);
-                // console.log(timeForShow);
-            } else if (minutesTmr == 0 && hoursTmr == 0 && daysTmr == 0 && monthsTmr > 0) {
-                --monthsTmr;
-                daysTmr = '30';
-                hoursTmr = '23';
-                minutesTmr = '59';
-                secondsTmr = '59';
-                emitterTimerConsole.emit('showRemainingTime', timeForShow);
-                // console.log(timeForShow);
-            } else if (minutesTmr == 0 && hoursTmr == 0 && daysTmr == 0 && monthsTmr == 0 && yearsTmr > 0) {
-                --yearsTmr;
-                monthsTmr = '12';
-                daysTmr = '30';
-                hoursTmr = '23';
-                minutesTmr = '59';
-                secondsTmr = '59';
-                emitterTimerConsole.emit('showRemainingTime', timeForShow);
-                // console.log(timeForShow);
-            }
-        } else {
-            --secondsTmr;
-            emitterTimerConsole.emit('showRemainingTime', timeForShow);
-            // console.log(timeForShow);
-        }
-    }, 1000);
-};
-
-setTimer();
+rl.on('close', () => {
+    console.log('Все');
+});
