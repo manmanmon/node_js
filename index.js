0d77b0c43a8e4ac590b853f2f81f240979762104
manmanmon <burgoch-roman9a@mail.ru>
2021-12-21 14:15:34 +0300

    fx

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const readStream = fs.createReadStream('./access.log', 'utf8');
 const writeStreamIP1 = fs.createWriteStream('./89.123.1.41.requests.log', 'utf8');
 const writeStreamIP2 = fs.createWriteStream('./34.48.240.111.requests.log', 'utf8');
 
-let firstIP = [];
-let secondIP = [];
-
 const rl = readline.createInterface({
     input: readStream,
     terminal: true,
@@ -18,19 +15,13 @@ rl.on('line', (line) => {
 
         switch (ip) {
             case '89.123.1.41':
-                firstIP.push(line);
                 writeStreamIP1.write(`${line}\n`);
                 break;
             case '34.48.240.111':
-                secondIP.push(line);
                 writeStreamIP2.write(`${line}\n`);
                 break;
             default:
                 break;
         }
     }
-});
-
-rl.on('close', () => {
-    console.log('Все');
 });
